250c48f1246ec1c6c0ab63dfb10f890eda3a8c4d
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2025-06-02 22:09:07 +0530

    Navbar code completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink>
+            <NavLink to='/' onClick={()=>setOpen(false)}>
                 <img className="h-9" src={assets.logo} alt="dummyLogoColored" />
             </NavLink>
 
@@ -29,14 +29,26 @@ const Navbar = () => {
                     <img src={assets.search_icon} alt="search" className='w-4 h-4' />
                 </div>
 
-                <div className="relative cursor-pointer">
+                <div onClick={()=>navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                {!user ? (
+                    <button onClick={()=>setShowUserlogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                     Login
                 </button>
+                )
+            :
+            (
+                <div className='relative group'>
+                    <img src={assets.profile_icon} className='w-10' alt="" />
+                    <ul className='hidden group-hover:block absolute top-10 right-0 bg-white  shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                        <li onClick={()=>navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                        <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                    </ul>
+                </div>
+            )}
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -45,7 +57,8 @@ const Navbar = () => {
             </button>
 
             {/* Mobile Menu */}
-            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+            { open && (
+                <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to='/' onClick={()=>setOpen(false)}>Home</NavLink>
                 <NavLink to='/products' onClick={()=>setOpen(false)}>All Products</NavLink>
                 {user && <NavLink to='/products' onClick={()=>setOpen(false)}>My Orders</NavLink>}
@@ -59,7 +72,7 @@ const Navbar = () => {
                 </button>)
                 }
                 
-            </div>
+            </div>)}
 
         </nav>
   )
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,10 +6,10 @@ export const AppContext = createContext();
 export const AppContextProvider  =({children})=>{
     const navigate = useNavigate();
     const [user,setUser] = useState(null);
-    const [isSeller,setIsSeller] = useState(null);
+    const [isSeller,setIsSeller] = useState(false);
     const [showUserLogin,setShowUserlogin] = useState(false);
 
-    const value={user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserlogin};
+    const value={user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserlogin,navigate};
 
     return <AppContext.Provider value={value}>
         {children}
