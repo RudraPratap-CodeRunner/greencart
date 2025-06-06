e0b771ab8200cb3bccdbb9fb035e194bd33d2012
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2025-06-06 22:14:34 +0530

    Bottom Banner added in Home page

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -9,7 +9,7 @@ const BestSeller = () => {
         <p className='text-2xl md:text-3xl font-medium text-left'>Best Sellers</p>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 mt-6'>
             {
-                products.filter((product)=>product.inStock).slice(0,5).map((product,index)=>(
+                products.filter((product)=>product.inStock).slice(0,4).map((product,index)=>(
                     <ProductCard key={index} product={product}/>
                 ))
             }
